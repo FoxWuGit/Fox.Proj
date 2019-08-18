56c60c10461747fb7557b9e8afd8dae60f3efed1
FoxWuGit <42526563+FoxWuGit@users.noreply.github.com>
2019-08-18 14:49:57 +0800

    (1)增加model及entityFramework的部份

---
+++ Fox.Model/Dao/Interface/IdbDao.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Fox.Model.Dao.Interface
+{
+    public interface IdbDao
+    {
+        IEnumerable<Student> StudentSelect();
+    }
+}
---
+++ Fox.Model/Dao/dbDao.cs
@@ -0,0 +1,23 @@
+﻿using Fox.Model.Dao.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fox.Model.Dao
+{
+    public class dbDao : IdbDao
+    {
+        dbEntities entities = new dbEntities();
+        public dbDao()
+        {
+
+        }
+
+        public IEnumerable<Student> StudentSelect()
+        {
+            return null;
+        }
+    }
+}
